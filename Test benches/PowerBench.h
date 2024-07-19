2c585ef2603fd773ecd43d3ceb2380ab2546773b
draksal1 <145187392+draksal1@users.noreply.github.com>
2024-07-19 13:25:20 +0700

    added classes implementation

--- Test benches/PowerBench.h
+++ Test benches/PowerBench.h
@@ -3,5 +3,15 @@
 
 class PowerBench : Bench
 {
+private:
+    double _timeStep;
+    double _lastMaxPower;
+    double _lastMaxRPM;
+public:
+    void TestEngine(Engine& engine) override;
+
+    const double GetMaxPower() const;
+
+    const double GetMaxRPM() const;
 };
 
